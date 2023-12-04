2b8b8be57b26845bcc69c894a13acf34ce881ed7
Feven-123 <151029966+Feven-123@users.noreply.github.com>
2023-12-04 04:42:56 -0800

    Add files via upload

---
+++ level1.js
@@ -0,0 +1,251 @@
+function myFunc() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/web2.html";
+}
+function myOne() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level1.html";
+}
+function myTwo() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level2.html";
+}
+function myThree() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level3.html";
+}
+function myFour() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level4.html";
+}
+function myFive() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level5.html";
+}
+function mySix() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level6.html";
+}
+function mySeven() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level7.html";
+}
+function myEight() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level8.html";
+}
+function myNine() {
+    var x=window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level9.html";
+}
+
+
+function  myBtn() {
+    var two = document.getElementById("2").value;
+    var four = document.getElementById("4").value;
+    var six = document.getElementById("6").value;
+
+    if(two==2) {
+        
+
+    } else {
+        document.write("1. wrong answer");
+    }
+    if(four==4) {
+
+    } else {
+        document.write("2. wrong answer");
+    }
+    if(six==6) {
+        var click = window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level2.html";
+        
+    } else {
+        document.write("3. wrong answer");
+    }
+    
+}
+function  myCtn() {
+    var eight = document.getElementById("8").value;
+    var ten = document.getElementById("10").value;
+    var twelve = document.getElementById("12").value;
+
+    if(eight==8) {
+        
+
+    } else {
+        document.write("1. wrong answer");
+    }
+    if(ten==10) {
+
+    } else {
+        document.write("2. wrong answer");
+    }
+    if(twelve==12) {
+        var click2 = window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level3.html";
+        
+    } else {
+        document.write("3. wrong answer");
+    }
+    
+}
+function  myDtn() {
+    var fourtn = document.getElementById("14").value;
+    var sixtn = document.getElementById("16").value;
+    var eightn = document.getElementById("18").value;
+
+    if(fourtn==14) {
+        
+
+    } else {
+        document.write("1. wrong answer");
+    }
+    if(sixtn==16) {
+
+    } else {
+        document.write("2. wrong answer");
+    }
+    if(eightn==18) {
+        var click2 = window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level4.html";
+        
+    } else {
+        document.write("3. wrong answer");
+    }
+    
+}
+function  myEtn() {
+    var aaa = document.getElementById("a").value;
+    var bbb = document.getElementById("b").value;
+    var ccc = document.getElementById("c").value;
+
+    if(aaa==1) {
+        
+
+    } else {
+        document.write("1. wrong answer");
+    }
+    if(bbb==4) {
+
+    } else {
+        document.write("2. wrong answer");
+    }
+    if(ccc==9) {
+        var click2 = window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level5.html";
+        
+    } else {
+        document.write("3. wrong answer");
+    }
+    
+}
+function  myFtn() {
+    var ddd = document.getElementById("d").value;
+    var eee = document.getElementById("e").value;
+    var fff = document.getElementById("f").value;
+
+    if(ddd==16) {
+        
+
+    } else {
+        document.write("1. wrong answer");
+    }
+    if(eee==25) {
+
+    } else {
+        document.write("2. wrong answer");
+    }
+    if(fff==36) {
+        var click2 = window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level6.html";
+        
+    } else {
+        document.write("3. wrong answer");
+    }
+    
+}
+function  myGtn() {
+    var ggg = document.getElementById("g").value;
+    var hhh = document.getElementById("h").value;
+    var iii = document.getElementById("i").value;
+
+    if(ggg==49) {
+        
+
+    } else {
+        document.write("1. wrong answer");
+    }
+    if(hhh==64) {
+
+    } else {
+        document.write("2. wrong answer");
+    }
+    if(iii==81) {
+        var click2 = window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level7.html";
+        
+    } else {
+        document.write("3. wrong answer");
+    }
+    
+}
+function  myHtn() {
+    var AAA = document.getElementById("A").value;
+    var BBB = document.getElementById("B").value;
+    var CCC = document.getElementById("C").value;
+
+    if(AAA==2) {
+        
+
+    } else {
+        document.write("1. wrong answer");
+    }
+    if(BBB==3) {
+
+    } else {
+        document.write("2. wrong answer");
+    }
+    if(CCC==4) {
+        var click2 = window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level8.html";
+        
+    } else {
+        document.write("3. wrong answer");
+    }
+    
+}
+function  myItn() {
+    var DDD = document.getElementById("D").value;
+    var EEE = document.getElementById("E").value;
+    var FFF = document.getElementById("F").value;
+
+    if(DDD==1) {
+        
+
+    } else {
+        document.write("1. wrong answer");
+    }
+    if(EEE==2) {
+
+    } else {
+        document.write("2. wrong answer");
+    }
+    if(FFF==3) {
+        var click2 = window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/level9.html";
+        
+    } else {
+        document.write("3. wrong answer");
+    }
+    
+}
+function  myJtn() {
+    var GGG = document.getElementById("G").value;
+    var HHH = document.getElementById("H").value;
+    var III = document.getElementById("I").value;
+
+    if(GGG==1) {
+        
+
+    } else {
+        document.write("1. wrong answer");
+    }
+    if(HHH==2) {
+
+    } else {
+        document.write("2. wrong answer");
+    }
+    if(III==3) {
+        var click2 = window.location.pathname="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/levelend.html";
+        
+    } else {
+        document.write("3. wrong answer");
+    }
+    
+} 
+function myKtn() {
+    var kk = window.location.pathname ="E:/FEVEN/New%20folder/pheaven/softwares/abcd/folder/project%202/web.html";
+}                                                                                                                                                                                                 
